23e898638b7b92d0fbcbe212792b8144cbaf0110
Anneke Kluft <anneke_kluft@sfu.ca>
2023-11-23 07:42:49 -0800

    Added rain timer

--- FinalProject/Assets/Rain.cs
+++ FinalProject/Assets/Rain.cs
@@ -7,6 +7,11 @@ public class Rain : MonoBehaviour
 
     private FlowerCounter flowerCounter;
     public GameObject Rain_Particle;
+    public bool hasRained = false;
+
+    // Timer
+    // https://stackoverflow.com/questions/73058385/making-a-5-second-timer-for-a-prefab
+    public float secsToRain;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +25,24 @@ public class Rain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Add to timer
+        secsToRain -= Time.deltaTime;
+
         // Check if flower2Count is more than 5 before activating Rain
         if (flowerCounter != null)
         {
             //Debug.Log("flower2Count: " + flowerCounter.flower2Count);  // Check the flower2Count value
 
-            if (flowerCounter.flower2Count >= 5)
+            if (flowerCounter.flower2Count >= 5 && !hasRained)
             {
                 Rain_Particle.gameObject.SetActive(true);
+                if (secsToRain <= 15)
+                {
+                    Rain_Particle.gameObject.SetActive(false);
+                    hasRained = true;
+
+                    // Insert code to grow bridge plant
+                }
             }
         }
     }
